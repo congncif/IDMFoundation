c7ef680100364248dfd2729f2b569addcba27bfa
Nguyen Chi Cong <congnc.if@gmail.com>
2018-03-01 10:20:21 +0700

    Update service template

--- IDM Foundation.xctemplate/BaseDataProvider/___FILEBASENAME___Service.swift
+++ IDM Foundation.xctemplate/BaseDataProvider/___FILEBASENAME___Service.swift
@@ -10,4 +10,9 @@ import Foundation
 import IDMFoundation
 import IDMCore
 
-typealias ___VARIABLE_moduleName___Service = MagicalIntegrator<___VARIABLE_moduleName___Provider, ___VARIABLE_moduleName___ResponseModel>
+class ___VARIABLE_moduleName___Service: MagicalIntegrator<___VARIABLE_moduleName___Provider, ___VARIABLE_moduleName___ResponseModel> {
+    init() {
+        super.init(dataProvider: ___VARIABLE_moduleName___Provider(), modelType: ___VARIABLE_moduleName___ResponseModel.self)
+    }
+}
+
