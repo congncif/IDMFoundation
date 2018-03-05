2fd0cf6045008203af2e55fed4a8a41cdb7240eb
NGUYEN CHI CONG <congnc.if@gmail.com>
2018-03-05 22:16:51 +0700

    Update installer

--- install-template.sh
+++ install-template.sh
@@ -1,2 +1,18 @@
-rm -rf ~/Library/Developer/Xcode/Templates/Project\ Templates/iOS/Application/IDM\ Foundation.xctemplate
+red=`tput setaf 1`
+green=`tput setaf 2`
+reset=`tput sgr0`
+
+app_dir=~/Library/Developer/Xcode/Templates/Project\ Templates/iOS/Application
+if [ ! -d "$app_dir" ] 
+then
+    mkdir "$app_dir"
+    echo "Creating new IDM-Foundation.xctemplate..."
+fi
+temp_dir=~/Library/Developer/Xcode/Templates/Project\ Templates/iOS/Application/IDM\ Foundation.xctemplate
+if [ -d "$temp_dir" ] 
+then
+	rm -rf "$temp_dir"
+	echo "Updating IDM-Foundation.xctemplate exists..."
+fi
 cp -R IDM\ Foundation.xctemplate ~/Library/Developer/Xcode/Templates/Project\ Templates/iOS/Application
+echo "${green}Installed IDM-Foundation.xctemplate successfully${reset}"
