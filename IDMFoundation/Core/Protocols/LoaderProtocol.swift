b50304e82d152efbcbb86c4fe745848f2b4c6f94
NGUYEN CHI CONG <congnc.if@gmail.com>
2018-11-09 10:23:39 +0700

    Added some defined protocols

--- IDMFoundation/Core/Protocols/LoaderProtocol.swift
+++ IDMFoundation/Core/Protocols/LoaderProtocol.swift
@@ -9,3 +9,5 @@ import Foundation
 import IDMCore
 
 public protocol LoaderProtocol: AnyObject, LoadingProtocol {}
+public protocol ErrorHandlerProtocol: AnyObject, ErrorHandlingProtocol {}
+public protocol IDMPresentationDelegate: AnyObject, LoadingProtocol, ErrorHandlingProtocol {}
