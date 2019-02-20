74a998ce5a54e245bc63d9ae65ec69eca337f3e0
CONG NGUYEN CHI <congnc.if@gmail.com>
2019-02-20 22:50:54 +0700

    Added `AnyValueDataProvider` Correctly `AnyResultDataProvider`

--- IDMFoundation/Core/Providers/AnyResultDataProvider.swift
+++ IDMFoundation/Core/Providers/AnyResultDataProvider.swift
@@ -8,17 +8,43 @@
 
 import Foundation
 import IDMCore
+import SiFUtilities
 
 public typealias ProviderResponseAny = (Bool, Any?, Error?)
 
 // An abstract class for generic declaring
-open class AnyResultDataProvider<ParameterType>: AbstractDataProvider<ParameterType, Any> {
-    @discardableResult
-    open override func request(parameters _: ParameterType?, completion _: @escaping (Bool, Any?, Error?) -> Void) -> CancelHandler? {
-        assertionFailure("Please override \(#function) to get data")
+public typealias AnyResultDataProvider<ParameterType> = AbstractDataProvider<ParameterType, Any>
+
+open class ValueDataProvider<ParameterType, ValueType>: AbstractDataProvider<ParameterType, ValueType> {
+    private var delay: TimeInterval
+    private var valueFactory: (ParameterType?) -> IDMCore.Result<ValueType>
+
+    public init(valueFactory: @escaping (ParameterType?) -> IDMCore.Result<ValueType>, delay: TimeInterval = 3) {
+        self.valueFactory = valueFactory
+        self.delay = delay
+    }
+
+    open override func request(parameters: ParameterType?, completion: @escaping (Bool, ValueType?, Error?) -> Void) -> CancelHandler? {
+        let _value = valueFactory(parameters)
+        let valueKeeper = ValueKeeper(defaultValue: nil, delay: delay) { fullfill in
+            fullfill(_value)
+        }
+        if let result = valueKeeper.syncValue {
+            switch result {
+            case .success(let data):
+                completion(true, data, nil)
+            case .failure(let error):
+                completion(false, nil, error)
+            }
+        } else {
+            completion(false, nil, nil)
+        }
+
         return nil
     }
 }
+
+open class AnyValueDataProvider<ParameterType>: ValueDataProvider<ParameterType, Any> {}
 
 // -------------------------------------------------------------------------
 
